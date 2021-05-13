d889e4c180f789238de65576db8283ff156e6dbf
IShungite <ishungite@gmail.com>
2021-05-13 11:20:54 +0200

    Updated signin / signup style

--- password_manager/src/components/Auth/Auth.tsx
+++ password_manager/src/components/Auth/Auth.tsx
@@ -2,8 +2,11 @@
 import { useState } from 'react';
 import { useDispatch } from 'react-redux';
 import { useHistory } from 'react-router-dom';
-import { Button, Grid, Typography, Container } from '@material-ui/core';
+import { Button, Grid, Typography, Container, Box, Avatar, TextField, Link, CssBaseline } from '@material-ui/core';
+import LockOutlinedIcon from '@material-ui/icons/LockOutlined';
 import { signin, signup } from '../../actions/auth';
+
+import useStyles from './styles';
 
 import Input from './Input';
 
@@ -15,11 +18,14 @@ const Auth = () => {
     const [formData, setFormData] = useState(initialState);
     const dispatch = useDispatch();
     const history = useHistory();
+    const classes = useStyles();
 
     const handleShowPassword = () => setShowPassword((prevShowPassword: Boolean) => !prevShowPassword)
 
     const handleSubmit = (e: any) => {
         e.preventDefault();
+
+        console.log(formData)
 
         if (isSignup) {
             dispatch(signup(formData, history));
@@ -36,30 +42,38 @@ const Auth = () => {
 
     return (
         <Container component="main" maxWidth="xs">
-            Auth
-            <Typography>{isSignup ? 'Sign Up' : 'Sign In'}</Typography>
-            <form onSubmit={handleSubmit}>
-                <Grid container spacing={2}>
-                    {isSignup && (
-                        <>
-                            <Input name="name" label="Name" handleChange={handleChange} autoFocus half />
-                        </>
-                    )}
-                    <Input name="email" label="Email Address" handleChange={handleChange} type="email" />
-                    <Input name="password" label="Password" handleChange={handleChange} type={showPassword ? "text" : "password"} handleShowPassword={handleShowPassword} />
-                    {isSignup && <Input name="confirmPassword" label="Repeat Password" handleChange={handleChange} type="password" />}
-                    <Button type="submit" fullWidth variant="contained" color="primary">
-                        {isSignup ? 'Sign Up' : 'Sign In'}
+            <CssBaseline />
+            <Box sx={{mt: 5,display: 'flex', flexDirection: 'column', alignItems: 'center'}}>
+                <Avatar sx={{ m: 1, bgcolor: 'secondary.main' }}>
+                    <LockOutlinedIcon />
+                </Avatar>
+                <Typography component="h1" variant="h5">{isSignup ? 'Sign up' : 'Sign In'}</Typography>
+
+                <Box component="form" onSubmit={handleSubmit} noValidate sx={{width: '100%', /* Fix IE11 issue. */ mt: 1,}}>
+                    {isSignup && <TextField id="name" name="name" label="Name" autoComplete="name" margin="normal" onChange={handleChange} required fullWidth autoFocus/>}
+
+                    <TextField id="email" name="email" label="Email Address" autoComplete="email" margin="normal" onChange={handleChange} required fullWidth autoFocus/>
+                    <TextField id="password" name="password" label="Password" type="password" autoComplete="current-password" margin="normal" onChange={handleChange} required fullWidth />
+                    {isSignup && <TextField id="confirmPassword" name="confirmPassword" label="Repeat Password" type="password" autoComplete="current-password" margin="normal" onChange={handleChange} required fullWidth />}
+                    <Button type="submit" fullWidth variant="contained" sx={{ mt: 3, mb: 2 }}>
+                        {isSignup ? 'Sign up' : 'Sign In'}
                     </Button>
-                    <Grid container alignItems="flex-end">
+
+                    <Grid container>
+                        <Grid item xs>
+                            <Link href="#" variant='body2'>
+                                Forgot password?
+                            </Link>
+                        </Grid>
                         <Grid item>
-                            <Button onClick={switchMode}>
+                            <Link href='#' variant='body2' onClick={() => {switchMode()}}>
                                 {isSignup ? 'Already have an account? Sign In' : 'Don\'t have an account? Sign up'}
-                            </Button>
+                            </Link>
                         </Grid>
                     </Grid>
-                </Grid>
-            </form>
+
+                </Box>
+            </Box>
         </Container>
     );
 };
