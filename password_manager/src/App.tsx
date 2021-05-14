ae6c9b7db9bfd939ad1038196b6e28c79d5184cb
IShungite <ishungite@gmail.com>
2021-05-14 19:00:56 +0200

    removed old header

--- password_manager/src/App.tsx
+++ password_manager/src/App.tsx
@@ -1,5 +1,4 @@
 import React from "react";
-import { Container } from "@material-ui/core";
 import { HashRouter, Switch, Route } from "react-router-dom";
 
 import Home from "./components/Home/Home";
@@ -8,13 +7,11 @@ import Auth from "./components/Auth/Auth";
 const App: React.FC = () => {
   return (
     <HashRouter>
-      {/* <Container maxWidth="lg"> */}
       <Switch>
         <Route exact path="/" component={Home} />
         {/* <Route exact path="/" component={Auth} /> */}
         <Route exact path="/home" component={Home} />
       </Switch>
-      {/* </Container> */}
     </HashRouter>
   );
 };
